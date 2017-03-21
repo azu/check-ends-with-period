a7fb083092bc139e158d9bfa28090ab8508553bb
azu <azuciao@gmail.com>
2017-03-21 23:02:32 +0900

    initial commit

---
+++ test/check-ends-with-period-test.js
@@ -0,0 +1,91 @@
+const assert = require("assert");
+const checkEndsWithPeriod = require("../src/check-ends-with-period");
+describe("check-ends-with-period", () => {
+    context("when period is end", () => {
+        it("should return valid:true result", () => {
+            const result = checkEndsWithPeriod("text.", {
+                periodMarks: ["."]
+            });
+            assert.deepEqual(result, {
+                valid: true,
+                periodMark: ".",
+                index: 4
+            });
+        });
+        it("multiple marks should return valid:true result", () => {
+            const result = checkEndsWithPeriod("テキスト。", {
+                periodMarks: [".", "。"]
+            });
+            assert.deepEqual(result, {
+                valid: true,
+                periodMark: "。",
+                index: 4
+            });
+        });
+    });
+    context("when whitespace is end", () => {
+        it("should return valid:false result", () => {
+            const result = checkEndsWithPeriod("text   ");
+            assert.deepEqual(result, {
+                valid: false,
+                periodMark: "   ",
+                index: 4
+            });
+        });
+    });
+    context("when not allowed mark is end", () => {
+        it("should return valid:false result", () => {
+            const result = checkEndsWithPeriod("text");
+            assert.deepEqual(result, {
+                valid: false,
+                periodMark: "t",
+                index: 3
+            });
+        });
+    });
+
+    context("when exclamation mark is end", () => {
+        it("allowExceptionMark:true and should return valid:true result", () => {
+            const result = checkEndsWithPeriod("text!", {
+                allowExceptionMark: true
+            });
+            assert.deepEqual(result, {
+                valid: true,
+                periodMark: "!",
+                index: 4
+            });
+        });
+        it("allowExceptionMark:false and should return valid:false result", () => {
+            const result = checkEndsWithPeriod("text!", {
+                allowExceptionMark: false
+            });
+            assert.deepEqual(result, {
+                valid: false,
+                periodMark: "!",
+                index: 4
+            });
+        });
+    });
+    context("when emoji is end", () => {
+        it("allowEmoji:true and should return valid:true result", () => {
+            const result = checkEndsWithPeriod("text❌", {
+                allowEmoji: true
+            });
+            assert.deepEqual(result, {
+                valid: true,
+                periodMark: "❌",
+                index: 4
+            });
+        });
+        it("allowEmoji:false and should return valid:false result", () => {
+            const result = checkEndsWithPeriod("text❌", {
+                allowEmoji: false
+            });
+            assert.deepEqual(result, {
+                valid: false,
+                periodMark: "❌",
+                index: 4
+            });
+        });
+    });
+});
